2fd04acbf7f095c60a5d08c743726a072650399d
Givsan Bhattarai <pyrexiagb@gmail.com>
2024-09-19 22:05:25 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading content:', error);
+        });
+}
